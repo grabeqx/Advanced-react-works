191ba05671983015b2ceca094d23fe6393d13b43
grabeqx <grabowski.kontakt@gmail.com>
2019-03-25 17:45:39 +0100

    Working with graphQL

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,12 @@
-const mutations = {};
+const Mutations = {
+  async createItem(parent, args, ctx, info) {
 
-module.exports = mutations;
+    const item = await ctx.db.mutation.createItem({
+      data: {...args}
+    }, info);
+    
+    return item;
+  }
+};
+
+module.exports = Mutations;
