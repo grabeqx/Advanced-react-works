00c49483c654fda73fce2858bdc7a8bdc323ed6c
grabeqx <grabowski.kontakt@gmail.com>
2019-04-11 14:58:08 +0200

    Show/Hide cart

--- frontend/components/Nav.jsx
+++ frontend/components/Nav.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Link from 'next/link';
+import {Mutation} from 'react-apollo';
 
 import NavStyles from './styles/NavStyles';
 import User from './User';
 import Signout from './Signout';
+import {TOGGLE_CART_MUTATION} from './Cart';
 
 const Nav = () => (
   <User>
@@ -31,6 +33,11 @@ const Nav = () => (
               <a>Sign in</a>
             </Link>
           )}
+          <Mutation mutation={TOGGLE_CART_MUTATION}>
+            {toggleCart => (
+              <button onClick={toggleCart}>My cart</button>
+            )}
+          </Mutation>
         </NavStyles>
       )}
     </User>
