03c4978412111b897ae7343075ad59d123854435
grabeqx <grabowski.kontakt@gmail.com>
2019-04-11 17:04:23 +0200

    Working witch cache

--- frontend/components/RemoveFromCart.jsx
+++ frontend/components/RemoveFromCart.jsx
@@ -24,11 +24,30 @@ const BigButton = styled.button`
 `;
 
 class RemoveFromCart extends React.Component {
+
+  update = (cache, payload) => {
+    const data = cache.readQuery({
+      query: CURRENT_USER_QUERY
+    });
+    const cartItemId = payload.data.removeFromCart.id;
+    data.me.cart = data.me.cart.filter(cartItem => cartItem.id !== cartItemId);
+    cache.writeQuery({query: CURRENT_USER_QUERY, data});
+  }
+
   render() {
     return (
       <Mutation mutation={REMOVE_FROM_CART_MUTATION} variables={{
         id: this.props.id
-      }} refetchQueries={[{query: CURRENT_USER_QUERY}]}>
+      }} 
+        update={this.update} 
+        optimisticResponse={{
+          __typename: 'Mutation',
+          removeFromCart: {
+            __typename: 'CartItem',
+            id: this.props.id
+          }
+        }}
+      >
         {(removeFromCart, {loading, error}) => (
           <BigButton title="Delete Item" onClick={removeFromCart}>
             &times;
