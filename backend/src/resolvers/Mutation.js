b0c63308f060d9eb725cc51488f5932782d1c404
grabeqx <grabowski.kontakt@gmail.com>
2019-04-11 16:20:58 +0200

    Removing item form cart

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -189,6 +189,22 @@ const Mutations = {
         }
       }
     }, info);
+  },
+  async removeFromCart(parent, args, ctx, info) {
+    const cartItem = await ctx.db.query.cartItem({
+      where: {
+        id: args.id
+      }
+    }, `{id, user {id}}`);
+    if(!cartItem) throw new Error('No Cart Item Found');
+    if(cartItem.user.id != ctx.request.userId) {
+      throw new Error('cheat');
+    }
+    return ctx.db.mutation.deleteCartItem({
+      where: {
+        id: args.id
+      }
+    }, info);
   }
 };
 
