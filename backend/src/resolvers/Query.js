969d3a33c6e6854963486304790a139a63f43d7c
grabeqx <grabowski.kontakt@gmail.com>
2019-04-15 17:12:41 +0200

    Displaying orders

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -22,6 +22,37 @@ const Query = {
     hasPermission(ctx.request.user, ['ADMIN', 'PERMISSIONUPDATE']);
 
     return ctx.db.query.users({}, info);
+  },
+  async order(parent, args, ctx, info) {
+    if (!ctx.request.userId) {
+      throw new Error('You arent logged in!');
+    }
+    const order = await ctx.db.query.order(
+      {
+        where: { id: args.id },
+      },
+      info
+    );
+    const ownsOrder = order.user.id === ctx.request.userId;
+    const hasPermissionToSeeOrder = ctx.request.user.permissions.includes('ADMIN');
+    if (!ownsOrder && !hasPermissionToSeeOrder) {
+      throw new Error('You cant see this buddd');
+    }
+    return order;
+  },
+  async orders(parent, args, ctx, info) {
+    const { userId } = ctx.request;
+    if (!userId) {
+      throw new Error('you must be signed in!');
+    }
+    return ctx.db.query.orders(
+      {
+        where: {
+          user: { id: userId },
+        },
+      },
+      info
+    );
   }
 };
 
